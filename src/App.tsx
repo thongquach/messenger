5964c26b7d4e9ae1ec5019c2fdf11e3f09d3ba45
quang.le <quang.ledinh@employmenthero.com>
2022-04-21 13:19:07 +0700

    define state for conversation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import AccountsSelection from './components/accountsSelection';
 import Conversations from './components/conversations';
-import { AccountType } from './utils/types';
+import { AccountType, ConversationType } from './utils/types';
 
 function App() {
   const [currAccount, setAccount] = useState<AccountType | undefined>();
+  const [currentConversation, setCurrentConversation] = useState<ConversationType | undefined>();
 
   if (currAccount === undefined) return <AccountsSelection setAccount={setAccount} />;
   return <Conversations account={currAccount} setAccount={setAccount} />;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,5 +2,19 @@ export type AccountType = {
   name: string;
   id: string;
 };
+export type Message = {
+  id: string;
+  text: string;
+  sender: AccountType;
+  createdAt: string;
+};
+
+export type ConversationType = {
+  id: string;
+  lastMessage: Message;
+  participants: AccountType[];
+};
 
 export type SetAccountType = (account: AccountType) => void;
+
+export type SetCurrentConversationType = (conversation: ConversationType) => void;
